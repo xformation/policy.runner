0620574ad49935907e8b068603f5dfdda1720737
Rajesh <rajeshupadhyay@girnarsoft.com>
2018-08-12 22:48:19 +0530

    Add feature implementation to merge the api call for dynamically storing policy entities.

--- src/main/java/com/synectiks/policy/runner/Application.java
+++ src/main/java/com/synectiks/policy/runner/Application.java
@@ -4,15 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @ComponentScan("com.synectiks")
 public class Application {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(Application.class);
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	private static ConfigurableApplicationContext ctx;
 
@@ -21,5 +22,10 @@ public class Application {
 		for (String bean : ctx.getBeanDefinitionNames()) {
 			logger.info("Beans: " + bean);
 		}
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void setIndexAndMapping() {
+		System.out.println("hello world, I have just started up");
 	}
 }
